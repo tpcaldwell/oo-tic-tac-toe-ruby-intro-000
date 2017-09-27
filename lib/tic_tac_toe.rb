75c647bde77f401cceb5693ff44995aca8b4e1a2
Thaddaeus Caldwell <tpcaldwell@gmail.com>
2017-09-27 19:31:45 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -18,4 +18,15 @@ class TicTacToe
     puts "-----------"
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
 end
+
