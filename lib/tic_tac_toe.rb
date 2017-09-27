43949df833c5924da36f552f413fa051cc285216
Thaddaeus Caldwell <tpcaldwell@gmail.com>
2017-09-27 19:53:42 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -41,4 +41,19 @@ end
 def valid_move?(index)
   index.between?(0,8) && !position_taken?(index)
 end
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(index)
+    move(index, current_player)
+    display_board
+  else
+    turn(@board)
+  end
+end
+
+
+
+
 end
