c00e6cebd8f60d9801ce12c76cc99b84a25d958f
Thaddaeus Caldwell <tpcaldwell@gmail.com>
2017-09-27 19:30:25 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,15 +2,15 @@ class TicTacToe
   def initialize(board = nil)
     @board = board || Array.new(9, " ")
   end
- 
+
   def current_player
     turn_count % 2 == 0 ? "X" : "O"
   end
- 
+
   def turn_count
     @board.count{|token| token == "X" || token == "O"}
   end
- 
+
   def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
