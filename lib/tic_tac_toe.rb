9fa95e47ff41bd8fbb29b9e0fda0cc41aaae16dc
Thaddaeus Caldwell <tpcaldwell@gmail.com>
2017-09-27 19:54:17 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -41,7 +41,7 @@ end
 def valid_move?(index)
   index.between?(0,8) && !position_taken?(index)
 end
-def turn(board)
+def turn
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
@@ -49,7 +49,7 @@ def turn(board)
     move(index, current_player)
     display_board
   else
-    turn(board)
+    turn
   end
 end
 
